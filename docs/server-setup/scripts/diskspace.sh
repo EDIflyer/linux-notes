d09c6c6f0060bee2f97fe32f130c9c3d162d96f5
EDIflyer <ajr@alanjrobertson.co.uk>
2023-04-07 14:16:05 +0100

    tweaks to diskspace script to add logging

--- docs/server-setup/scripts/diskspace.sh
+++ docs/server-setup/scripts/diskspace.sh
@@ -4,16 +4,16 @@
 # Original script: https://www.cyberciti.biz/tips/shell-script-to-watch-the-disk-space.html
 # Edited by: Alan J Robertson
 
-ALERT=80 # alert level
+printf "diskmonitor.sh run on $(date) - " >> /home/alan/disk.log
+ALERT=75 # alert level
 ADMIN="ajr@alanjrobertson.co.uk" # dev/sysadmin email ID
 df -H /dev/sda | grep sda | awk '{ print $5 " " $1 }' | while read -r output;
 do
-  echo "$output"
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1 )
+  echo "currently used percentage = $usep%." >> /home/alan/disk.log
   partition=$(echo "$output" | awk '{ print $2 }' )
   if [ $usep -ge $ALERT ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)"
-    # remember %% required with printf as % is a special character
+    echo "Warning email sent $(date) as running out of space \"$partition ($usep%)\" on $(hostname)" >> /home/alan/disk.log
     printf "Subject: Disk space warning\n\nAlert: Almost out of disk space on Linode server - $usep%% used" | msmtp -d -a default "$ADMIN"
   fi
 done
